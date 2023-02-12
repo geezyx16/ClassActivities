26214fcd7df5632d1b508961611e4a4617178988
Gabriel Gyimah <ggyimah24@gmail.com>
2023-02-13 01:13:01 +0300

    edited 5.2.2.Map.js file

--- Section5/5.2/5.2.2-Map.js
+++ Section5/5.2/5.2.2-Map.js
@@ -233,8 +233,18 @@ const data = [
   }
 ];
 
-// using map, create a list of cities from the data
-
+let cityArray = data.map(item => item.address.city);
+// console.log(cityArray);
 // create an array of latitude and longitude coordinate pairs
+const coords = data.map(item => {
+  let lat = item.address.geo.lat;
+  let lng = item.address.geo.lng;
 
+  return `${lat}, ${lng}`;
+});
+// console.log(coords);
 // create an array of strings that combine the company's catchPhrase and bs
+const jargon = data.map(
+  item => `${item.company.catchPhrase} ${item.company.bs}`
+);
+console.log(jargon);
